83916e646fc102ea3b4e718b22e534d91b1b3a05
manozd <mansur.ozdemirov97@gmail.com>
2021-02-01 11:14:17 +0300

    Update setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="gym_linkage",
+    name="lifting-simulation-rl",
     version="0.0.1",
-    install_requires=["gym", "sympy"],  # And any other dependencies foo needs
+    install_requires=[
+        "gym",
+        "sympy",
+        "scipy",
+        "tensorflow==2.2.0",
+        "keras==2.3.1",
+        "hydra-core",
+    ],
 )
