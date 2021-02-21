1e747e844bfbaaaca2fad014bf72a1311195d5cb
peter.zaidel <p.zaidel@corp.mail.ru>
2021-02-21 13:58:21 +0300

    interpolating trajectory

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="lifting-simulation-rl",
+    name="lifting_rl",
     version="0.0.1",
-    install_requires=[
-        "gym",
-        "sympy",
-        "scipy",
-        "keras-rl",
-        "keras==2.2.4",
-        "tensorflow==1.13.1",
-    ],
+    root='./',
+#    install_requires=[
+#        "gym",
+#        "sympy",
+#        "scipy",
+#        "keras-rl",
+#        "keras==2.2.4",
+#        "tensorflow==1.13.1",
+#    ],
 )
